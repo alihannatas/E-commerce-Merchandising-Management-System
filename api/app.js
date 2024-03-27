f9e667c3da6520890353bc6ad3a0200d3cc9b949
alihannatas <alihan2644@gmail.com>
2024-03-27 18:41:32 +0300

    cors middleware added.

--- api/app.js
+++ api/app.js
@@ -11,6 +11,12 @@ const adminRouter = require("./routes/admin");
 const app = express();
 
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use("/api", adminRouter);
 
